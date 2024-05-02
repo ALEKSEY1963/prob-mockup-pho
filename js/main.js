d8085d759c87a333ab138591563d3ff972f5ddee
“ALEKSEY1963 <ab63@list.ru>
2024-05-02 16:36:57 +0300

    1_02.05

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+
+
+
+
 const burger = document.getElementById("burger");
 const nav = document.getElementById("nav");
+
+
 
 burger.addEventListener("click", function () {
   nav.classList.toggle("nav--active");
